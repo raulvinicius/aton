90d8c96b500b955cbfc6efbf362f645d8a2849eb
Raul Vinicius <raul.portifolio@gmail.com>
2015-02-25 03:27:17 -0300

    configuracao do boilerplate / bootstrap e primeiros elementos da home

--- wp-content/themes/aton/frontend/index.php
+++ wp-content/themes/aton/frontend/index.php
@@ -1,8 +1,57 @@
 <?php get_header(); ?>
+asdfasdfa
+
+<section class="chamada">
+
+	<hgroup>
+		<h2>Sua casa fora de casa</h2>
+		<h3>Conforto e tranquilidade em Goiânia</h3>
+	</hgroup>
+
+	<form>
+		<div class="form-group">
+			<label for="nome-reserva">Nome</label>
+			<input type="text" class="form-control" id="nome-reserva">
+		</div>
+		<div class="form-group">
+			<label for="telefone-reserva">Telefone</label>
+			<input type="phone" class="form-control" id="telefone-reserva">
+		</div>
+		<div class="form-group">
+			<label for="email-reserva">E-mail</label>
+			<input type="email" class="form-control" id="email-reserva">
+		</div>
+		<div class="form-group">
+			<label for="entrada-reserva">Entrada</label>
+			<input type="date" class="form-control" id="entrada-reserva">
+		</div>
+		<div class="form-group">
+			<label for="saida-reserva">Saída</label>
+			<input type="date" class="form-control" id="saida-reserva">
+		</div>
+		<button type="submit" class="btn btn-default">Fazer minha reserva</button>
+	</form>
+
+</section>
 
 
 
-Oi! Sou a página inicial do frontend.
+
+</body>
+
+
+
+
+      <footer>
+        <p>&copy; Company 2015</p>
+      </footer>
+
+
+
+
+
+
+
 
 
 
--- wp-content/themes/aton/header.php
+++ wp-content/themes/aton/header.php
@@ -1,8 +1,70 @@
 <!-- CÓDIGO HTML VEM AQUI -->
 
 
+<?php if ( strpos( $_SERVER[ "REQUEST_URI" ], "frontend" ) ) : ?>
 
-<p>I'm header</p>
+    <?php $frontendUrl = "/frontend"; //variável usada nos urls dentro do ambiente de desenvolvimento frontend?>
+
+<?php endif; ?>
+
+
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Aton Plaza Hotel</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 50px;
+                padding-bottom: 20px;
+            }
+        </style>
+        <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/style.css">
+
+        <script src="<?php bloginfo('template_url') ?>/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div class="">
+            <div class="navbar-header">
+    			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+    				<span class="sr-only">Toggle navigation</span>
+    				<span class="icon-bar"></span>
+    				<span class="icon-bar"></span>
+    				<span class="icon-bar"></span>
+    			</button>
+    			<a class="navbar-brand main-logo" href="<?php bloginfo('url'); echo $frontendUrl ?>"><h1>Aton Plaza Hotel</h1></a>
+            </div>
+
+            <div id="navbar" class="navbar-collapse collapse">
+    			<ul class="nav navbar-nav">
+    		        <li><a href="<?php bloginfo('url'); echo $frontendUrl ?>/o-hotel">O Hotel</a></li>
+    		        <li><a href="<?php bloginfo('url'); echo $frontendUrl ?>/acomodacoes">Acomodações</a></li>
+    		        <li><a href="<?php bloginfo('url'); echo $frontendUrl ?>/servicos">Serviços</a></li>
+    		        <li><a href="<?php bloginfo('url'); echo $frontendUrl ?>/reservas">Reservas</a></li>
+    		        <li><a href="<?php bloginfo('url'); echo $frontendUrl ?>/tarifas">Tarifas</a></li>
+    		        <li><a href="<?php bloginfo('url'); echo $frontendUrl ?>/promocoes">Promoções</a></li>
+    		        <li><a href="<?php bloginfo('url'); echo $frontendUrl ?>/parceiros">Parceiros</a></li>
+    		        <li><a href="<?php bloginfo('url'); echo $frontendUrl ?>/contato">Contato</a></li>
+    	      </ul>
+            </div><!--/.navbar-collapse -->
+          </div>
+        </nav>
 
 
 
@@ -26,7 +88,7 @@ endif;
 
 ?>
 
-<?php if ( strpos( $_SERVER[ "REQUEST_URI" ], "frontend" ) ) : ?>
+<?php if ( $frontendUrl != "" ) : ?>
 
     <?php $tUrl = get_bloginfo("template_url"); ?>
 
