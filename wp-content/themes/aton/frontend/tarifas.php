22876b2453ad228aa526952005ad64ab4539d7e8
Raissa Costa <raissacostaesilva@gmail.com>
2015-03-05 20:44:42 -0300

    atualizacao

--- wp-content/themes/aton/frontend/tarifas.php
+++ wp-content/themes/aton/frontend/tarifas.php
@@ -1,8 +1,28 @@
 <?php get_header(); ?>
 
+<section id="tarifas" class="compensa-menu">
+
+	<div class="container-fluid">
+		<div class="row">
+
+			<ul>
+				<li id="individual" class="col-sm-4">
+					<header><h2>Individual</h2></header>
 
 
-<p>FRONTEND: Tarifas</p>
+				</li>
+
+			</ul>
+
+
+		</div>
+
+	</div>
+
+
+
+</section>
+
 
 
 
