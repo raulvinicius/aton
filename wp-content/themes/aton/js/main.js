60e94b5fb7bae3ecf2c65ace60235940506f877e
Raul Vinicius <raul.portifolio@gmail.com>
2015-03-05 23:51:59 -0300

    frontend reservas: OK, frontend contato OK

--- wp-content/themes/aton/js/main.js
+++ wp-content/themes/aton/js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 
-    
+	$('.navbar a.contato').bind( 'click', function(e){
+		e.preventDefault;
+		$('#contato').show();
+		return false;
+	} );
+
+	$('.navbar a.reservas').bind( 'click', function(e){
+		e.preventDefault;
+		$('#reservas').show();
+		return false;
+	} );
+
+	$('.fechar-modal-form, .click-trap').bind( 'click', function(e){
+		$(this).closest('section').hide();
+		// $(this).closest('form')[0].reset();
+		return false;
+	} );
+
+	$('.aton-form').bind( 'click', function(e){
+		return false;
+	} );
 
 });
