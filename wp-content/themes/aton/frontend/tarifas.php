9a55e10586b4add6a8f28a6714874148014c14b9
Raul Vinicius <raul.portifolio@gmail.com>
2015-03-08 01:52:37 -0300

    responsivo tarifas OK

--- wp-content/themes/aton/frontend/tarifas.php
+++ wp-content/themes/aton/frontend/tarifas.php
@@ -7,31 +7,33 @@
 		<div class="row">
 
 			<ul>
-				<div class="col-sm-4">
+				<div class="col-md-4 col-sm-6">
 					<li id="individual" class="quartos-tarifas">
 
-						<header><h2>Individual</h2></header>
+						<header>
+							<h2>Quarto quádruplo</h2>
+						</header>
 
 						<div class="container-fluid">
 
 							<ul>
 								<li class="row">
-									<h3 class="col-sm-6">Adultos</h3>
+									<h3 class="col-xs-6">Adultos</h3>
 									<p class="for-spider">1</p>
-									<figure class="col-sm-6 right-align">
+									<figure class="col-xs-6 right-align">
 										<img src="<?php bloginfo('template_url') ?>/img/aton-icon-adulto.png">
 									</figure>
 								</li>
 								<li class="row">
-									<h3 class="col-sm-6">Criança (menos de 07 anos)</h3>
-									<figure class="col-sm-6 right-align">
+									<h3 class="col-xs-6">Criança (menos de 07 anos)</h3>
+									<figure class="col-xs-6 right-align">
 										<img src="<?php bloginfo('template_url') ?>/img/aton-nenhum-tarifas.jpg">
 									</figure>
 								</li>
 								<li class="row camas">
-									<h3 class="col-sm-6">Camas</h3>
+									<h3 class="col-xs-6">Camas</h3>
 									<p class="for-spider">01 solteiro</p>
-									<figure class="col-sm-6 right-align">
+									<figure class="col-xs-6 right-align">
 										<img src="<?php bloginfo('template_url') ?>/img/aton-icon-cama-solteiro.png">
 									</figure>
 								</li>
@@ -40,12 +42,12 @@
 
 							<div class="row tarifa-net">
 
-								<div class="col-sm-6">
+								<div class="col-xs-6">
 									<h3>Tarifa Internet</h3>
 									<span>Preços exclusivos para reservas feitas pelo site</span>
 								</div>
 
-								<div class="col-sm-6 right-align">
+								<div class="col-xs-6 right-align">
 									<p><span>R$</span>105<span>,00</span></p>
 								</div>
 
@@ -53,12 +55,12 @@
 
 							<div class="row tarifa-balcao">
 
-								<div class="col-sm-6">
+								<div class="col-xs-6">
 									<h3>Tarifa Balcão</h3>
 									<span>Preço para diárias contratadas na recepção do hotel.</span>
 								</div>
 
-								<div class="col-sm-6 right-align">
+								<div class="col-xs-6 right-align">
 									<p><span>R$</span>105<span>,00</span></p>
 								</div>
 
@@ -69,20 +71,20 @@
 
 
 						<div class="wrap-verificar">
-							<button class="verificar col-sm-12">Verificar disponibilidade</button>
+							<button class="verificar col-xs-12">Verificar disponibilidade</button>
 
-							<div class="col-sm-12">
+							<div class="col-xs-12">
 
 								<div class="row quartos-disponiveis">
-									<p class="col-sm-8"><strong>03</strong> quartos para verificar, <strong>quantos você quer reservar?</strong></p>
+									<p class="col-xs-7"><strong>03</strong> quartos para verificar, <strong>quantos você quer reservar?</strong></p>
 
-									<div class="col-sm-2 quartos-quantidade">
+									<div class="col-xs-2 alpha omega quartos-quantidade">
 									    <button class="seta-up"><img src="<?php bloginfo('template_url') ?>/img/aton-icon-seta-cima-tarifas.png"></button>
 									    <span>01</span>
 									    <button class="seta-down"><img src="<?php bloginfo('template_url') ?>/img/aton-icon-seta-baixo-tarifas.png"></button>
 									</div>
 
-									<div class="col-sm-2 wrap-ok">
+									<div class="col-xs-3 wrap-ok">
 										<button class="bt-ok">OK</button>
 									</div>
 								</div>
@@ -91,11 +93,55 @@
 
 						</div>
 
-				<div class="clearfix"></div>
+						<div class="clearfix"></div>
 
 
 					</li>
+
 				</div>
+
+				<div class="col-md-4 col-sm-6">
+					<li id="individual" class="quartos-tarifas cama-extra">
+
+						<header><h2>Individual</h2></header>
+
+						<div class="container-fluid">
+
+							<div class="row tarifa-net">
+
+								<div class="col-xs-6">
+									<h3>Tarifa Internet</h3>
+									<span>Preços exclusivos para reservas feitas pelo site</span>
+								</div>
+
+								<div class="col-xs-6 right-align">
+									<p><span>R$</span>105<span>,00</span></p>
+								</div>
+
+							</div>
+
+							<div class="row tarifa-balcao">
+
+								<div class="col-xs-6">
+									<h3>Tarifa Balcão</h3>
+									<span>Preço para diárias contratadas na recepção do hotel.</span>
+								</div>
+
+								<div class="col-xs-6 right-align">
+									<p><span>R$</span>105<span>,00</span></p>
+								</div>
+
+							</div>
+
+						</div>
+
+						<div class="clearfix"></div>
+
+
+					</li>
+
+				</div>
+
 
 				<div class="clearfix"></div>
 
